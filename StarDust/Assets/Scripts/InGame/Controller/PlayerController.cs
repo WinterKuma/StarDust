310d620cb4b181da77bc33dfdf9c5c0380641eae
WinterKuma <equsejhy@naver.com>
2021-09-27 16:04:32 +0900

    1단 판정 추가

--- StarDust/Assets/Scripts/InGame/Controller/PlayerController.cs
+++ StarDust/Assets/Scripts/InGame/Controller/PlayerController.cs
@@ -9,12 +9,12 @@ public class PlayerController : MonoBehaviour
     //fsm으로 나중에 바꿀 것
     bool isMove = false;
     int targetX, targetY, targetZ;
-    Tilemap map;
+    public Tilemap map;
+    public Tilemap map2;
 
     // Start is called before the first frame update
     void Start()
     {
-        map = GameObject.FindGameObjectWithTag("Map").GetComponent<Tilemap>();
     }
 
     // Update is called once per frame
@@ -31,15 +31,42 @@ public class PlayerController : MonoBehaviour
                 Vector2 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
                 map.RefreshAllTiles();
+                map2.RefreshAllTiles();
 
                 int x, y, z;
-                x = map.WorldToCell(point).x;
-                y = map.WorldToCell(point).y;
-                z = map.WorldToCell(point).z;
+                Vector3 p = new Vector3(point.x, point.y, 1);
+                Vector3Int v;
+                bool isTile = false;
 
-                Debug.LogFormat("{0}, {1}, {2}", x, y, z);
+                x = map2.WorldToCell(p).x;
+                y = map2.WorldToCell(p).y;
+                z = map2.WorldToCell(p).z;
+                v = new Vector3Int(x, y, z);
 
-                SetMove(x, y, z);
+                if (map2.GetTile(v) == null)
+                {
+                    p.z = 0;
+                    x = map.WorldToCell(point).x;
+                    y = map.WorldToCell(point).y;
+                    z = map.WorldToCell(point).z;
+                    v = new Vector3Int(x, y, z);
+                }
+                else
+                {
+                    Debug.LogFormat("{0}, {1}, {2}", x, y, z);
+
+                    SetMove(x, y, z);
+                    return;
+                }
+                if(map.GetTile(v) == null)
+                {
+                }
+                else
+                {
+                    Debug.LogFormat("{0}, {1}, {2}", x, y, z);
+
+                    SetMove(x, y, z);
+                }
             }
         }
     }
@@ -54,14 +81,15 @@ public class PlayerController : MonoBehaviour
 
     void Moving()
     {
-        transform.position = map.CellToWorld(new Vector3Int(targetX, targetY, 0));
+        transform.position = map.CellToWorld(new Vector3Int(targetX, targetY, targetZ));
 
         int x = map.WorldToCell(transform.position).x;
         int y = map.WorldToCell(transform.position).y;
 
+
         if (x == targetX && y == targetY)
         {
-            transform.position = map.CellToWorld(new Vector3Int(targetX, targetY, 1));
+            transform.position = map.CellToWorld(new Vector3Int(targetX, targetY, targetZ + 1));
             isMove = false;
         }
     }
